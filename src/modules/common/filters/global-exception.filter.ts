c2de47c01fc4ff3c4eb3b3f4160008d7ea201f65
lokeshboopalan <lokeshb@tecdata.co.in>
2026-05-21 10:22:48 +0530

    test

--- src/modules/common/filters/global-exception.filter.ts
+++ src/modules/common/filters/global-exception.filter.ts
@@ -9,6 +9,7 @@ import {
 import { Request, Response } from 'express';
 import { QueryFailedError } from 'typeorm';
 
+
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(GlobalExceptionFilter.name);
